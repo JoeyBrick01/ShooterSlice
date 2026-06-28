c0644a069de018b6445dd23f4c44b20fc0717cb3
Joey Brick <JBrick01@yahoo.com>
2026-06-28 13:40:34 -0400

    added a Utility Manager and created the negative correction function

---
+++ ShooterSlice/UtilMan.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ShooterSlice;
+
+public static class UtilMan
+{
+    /// <summary>
+    /// Corrects negative values to zero. If the input value is negative, it returns 0; otherwise, it returns the original value.
+    /// </summary>
+    /// <param name="value">The integer value to correct.</param>
+    /// <returns>The corrected integer value.</returns>
+    public static int NegCorrect(int value)
+    {
+        if (value < 0) {
+            return 0;
+        }
+        return value;
+    }
+}
