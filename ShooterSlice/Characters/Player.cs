857db972a133ba08d8b56e19eef9b6d340e08986
Joey Brick <JBrick01@yahoo.com>
2026-06-29 17:49:56 -0400

    added player class and made it drawable

---
+++ ShooterSlice/Characters/Player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Graphics;
+using MonoGame.Extended.Input;
+
+namespace ShooterSlice.Characters;
+
+public class Player
+{
+    private Sprite _sprite;
+    private readonly Vector2 Origin;
+    public Vector2 Position;
+    public int Speed;
+    public int Rotation;
+    private Vector2 _scale;
+
+    public Player(Sprite sprite, Vector2 pos, Vector2 scale)
+    {
+        _sprite = sprite;
+        Position = pos;
+        _scale = scale;
+
+        Origin = new Vector2(sprite.TextureRegion.X / 2, sprite.TextureRegion.Y / 2);
+        _sprite.Origin = Origin;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        KeyboardStateExtended keyboard = KeyboardExtended.GetState();
+        MouseStateExtended mouse = MouseExtended.GetState();
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        // SpriteBatch, Sprite, Position, Rotation, Scale
+        SpriteBatchExtensions.Draw(spriteBatch, _sprite, Position, Rotation, _scale);
+    }
+
+}
--- ShooterSlice/Scenes/TestENV.cs
+++ ShooterSlice/Scenes/TestENV.cs
@@ -7,6 +7,7 @@ using MonoGame.Extended.Graphics;
 using MonoGame.Extended.Screens;
 using MonoGame.Extended.Screens.Transitions;
 using Microsoft.Xna.Framework.Media;
+using ShooterSlice.Characters;
 
 namespace ShooterSlice.Scenes;
 
@@ -17,6 +18,8 @@ public class TestENV: GameScreen
 
     Texture2D _tile;
     Song _sceneSong;
+
+    Player _player;
 
     public TestENV(Game game) : base(game)
     {
@@ -32,12 +35,18 @@ public class TestENV: GameScreen
 
         _sceneSong = game.Content.Load<Song>("audio/castlevania");
         game.PlaySong(_sceneSong);
+
+        // Player Creation
+        Texture2D playerTexture = game.Content.Load<Texture2D>("images/TDPlayer");
+        Sprite playerSprite = new Sprite(playerTexture);
+        _player = new(playerSprite, Vector2.Zero, new Vector2(4, 4));
     }
 
     public override void Update(GameTime gameTime)
     {
         KeyboardStateExtended keyboard = KeyboardExtended.GetState();
         MouseStateExtended mouse = MouseExtended.GetState();
+
         // nav to title
         if (keyboard.WasKeyPressed(Keys.Escape))
         {
@@ -54,7 +63,9 @@ public class TestENV: GameScreen
         _spriteBatch.Draw(_tile, game.GraphicsDevice.PresentationParameters.Bounds, new Rectangle(Vector2.Zero.ToPoint(), game.GraphicsDevice.PresentationParameters.Bounds.Size), Color.White);
         _spriteBatch.End();
 
-        _spriteBatch.Begin();
+        // Actual scene drawing
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+        _player.Draw(_spriteBatch);
 
         _spriteBatch.End();
     }
